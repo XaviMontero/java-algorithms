f478a89dbbcc5ad2454808ab189ec19f71b14fe6
XaviMontero <oxmonteroc83@est.ucacue.edu.ec>
2024-03-13 20:24:44 -0500

    The aggregation process of the summingInt collector

--- src/main/java/com/kp/algorithms/Collector.java
+++ src/main/java/com/kp/algorithms/Collector.java
@@ -3,10 +3,12 @@ package com.kp.algorithms;
 import java.util.ArrayList;
 
 import java.util.HashMap;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.kp.objects.restaurant.Dish;
 import com.kp.objects.transaction.Currency;
 import com.kp.objects.transaction.Transaction;
 
@@ -29,6 +31,35 @@ public class Collector {
 
       listMap.get(new Currency("USD", "US Dollar")).forEach(System.out::println);
 
+      Long count = listData.stream().collect(Collectors.counting());
+      Long reduce = listData.stream().count();
+      System.out.println(count);
+      System.out.println(reduce);
+      IntSummaryStatistics summaryStatistics = getListDishe().stream().collect(Collectors.summarizingInt(Dish::getCalories));
+
+      System.out.println(summaryStatistics.getMax());
+      System.out.println(summaryStatistics.getMin());
+      System.out.println(summaryStatistics.getAverage());
+      System.out.println(summaryStatistics.getCount());
+      System.out.println(summaryStatistics.getSum());
+
+
    }
 
+   private static List<Dish> getListDishe() {
+      List<Dish> dishes = new ArrayList<>();
+      dishes.add(new Dish("pork", 800, false));
+      dishes.add(new Dish("beef", 700, false));
+      dishes.add(new Dish("chicken", 400, false));
+      dishes.add(new Dish("french fries", 530, true));
+      dishes.add(new Dish("rice", 350, true));
+      dishes.add(new Dish("season fruit", 120, true));
+      dishes.add(new Dish("rice", 350, true));
+      dishes.add(new Dish("season fruit", 120, true));
+      dishes.add(new Dish("salad", 220, true));
+      dishes.add(new Dish("pizza", 550, true));
+      dishes.add(new Dish("prawns", 300, false));
+      dishes.add(new Dish("salmon", 450, false));
+      return dishes;
+   }
 }
