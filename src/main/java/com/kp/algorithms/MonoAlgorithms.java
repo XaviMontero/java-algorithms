07246930fc994388656bc715c8c312f86a2d639f
XaviMontero <oxmonteroc83@est.ucacue.edu.ec>
2024-03-21 21:24:44 -0500

    Asynchronous and Non-Blocking

--- src/main/java/com/kp/algorithms/MonoAlgorithms.java
+++ src/main/java/com/kp/algorithms/MonoAlgorithms.java
@@ -7,18 +7,59 @@ import reactor.core.publisher.Mono;
 public class MonoAlgorithms {
 
    public static void main(String[] args) {
-
+/*
       //data error
       Integer fin = "data".length() / 0;
       System.out.println(fin);
       //data error
+*/
 
 
       Mono<Integer> mono = Mono.just("data").map(String::length).map(l -> l / 0);
 
       mono.subscribe(System.out::println, System.out::println, () -> System.out.println("Done"));
       mono.subscribe(Util.onNext(), Util.onError(), Util.onComplete());
-
       System.out.println("End of main");
+    testMono(2).subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+
+    testMono();
+      testMono().subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+    testMono();
+
+    System.out.println("============================");
+
+   Mono.fromRunnable(sendEmail()).subscribe(Util.onNext(), Util.onError(),
+
+         () -> System.out.println("Email sent"));
+   }
+
+
+   public static Mono<String> testMono() {
+      System.out.println("Start generating name");
+      return Mono.fromSupplier(() -> {
+         Util.sleepSeconds(3);
+         System.out.println("Generating name");
+        return Util.getFaker().name().fullName();
+      });
+   }
+   public static Mono<String> testMono(int i) {
+
+      Mono<String> userRepository = Mono.just(Util.getFaker().name().fullName());
+
+      if (i == 1){
+         userRepository = userRepository.map(String::toUpperCase);
+      }else if (i == 2){
+         return Mono.error(new RuntimeException("Error"));
+      }
+      return userRepository;
+
+
+   }
+
+   public  static Runnable sendEmail(){
+      return () -> {
+         Util.sleepSeconds(3);
+         System.out.println("Sending email to ");
+      };
    }
 }
--- src/main/java/com/kp/util/Util.java
+++ src/main/java/com/kp/util/Util.java
@@ -3,7 +3,11 @@ package com.kp.util;
 import java.util.Objects;
 import java.util.function.Consumer;
 
+import com.github.javafaker.Faker;
+
 public class Util {
+
+   public static Faker FAKER = new Faker();
 
    public static Consumer<Object> onNext() {
       return item -> System.out.println(item);
@@ -15,5 +19,17 @@ public class Util {
 
    public static Runnable onComplete() {
       return () -> System.out.println("Done");
+   }
+
+   public static Faker getFaker() {
+      return FAKER;
+   }
+
+   public static void sleepSeconds(int seconds) {
+      try {
+         Thread.sleep(seconds * 1000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
    }
 }
