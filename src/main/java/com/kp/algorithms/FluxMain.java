179d2a465073b331f06b6e8df9d67dc67ad02ba1
XaviMontero <oxmonteroc83@est.ucacue.edu.ec>
2024-03-26 21:50:18 -0500

    Flux

---
+++ src/main/java/com/kp/algorithms/FluxMain.java
@@ -0,0 +1,22 @@
+package com.kp.algorithms;
+
+import com.github.javafaker.Faker;
+import com.kp.util.Util;
+
+import reactor.core.publisher.Flux;
+
+public class FluxMain {
+
+   public static void main(String[] args) {
+
+      Flux<Object> flux = Flux.just(1, "Hola", 3, 4, Util.getFaker().name().fullName(), 6, 8, 9, 10);
+
+      flux.subscribe(Util.onNext());
+System.out.println("Filtering only integers");
+      Flux<Integer> flux1 =  flux.filter(Integer.class::isInstance).cast(Integer.class);
+      flux1.subscribe(Util.onNext());
+
+
+   }
+
+}
